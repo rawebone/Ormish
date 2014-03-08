b1f04b3b8c5563e242331407dd00bbfdb1d34d6b
Nick Rawe <nickrawe2@gmail.com>
2014-03-08 13:35:31 +0000

    Gateway Improvments, part 1
    Added the EntityManager object which will provide handling for common
    Entity tasks and remove some of the complexity from the Gateways.

---
+++ library/Rawebone/Ormish/EntityManager.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Rawebone\Ormish;
+
+/**
+ * Provides a wrapper over the Entity management tools in the library, allowing
+ * for easier access to data and caching.
+ */
+class EntityManager
+{
+    protected $mdm;
+    protected $defaults;
+    protected $objects;
+    protected $cachedDefaults = array();
+    
+    public function __construct(DefaultsCreator $defaults, MetaDataManager $mdm,
+        ObjectCreator $objects)
+    {
+        $this->defaults = $defaults;
+        $this->mdm = $mdm;
+        $this->objects = $objects;
+    }
+
+    /**
+     * Returns the default field values for an Entity.
+     * 
+     * @param string $name
+     * @return array
+     */
+    public function defaults($name)
+    {
+        if (!isset($this->cachedDefaults[$name])) {
+            $this->cachedDefaults[$name] = $this->defaults->make($this->metadata($name));
+        }
+        
+        return $this->cachedDefaults[$name];
+    }
+
+    /**
+     * Returns the metadata for an Entity.
+     * 
+     * @param string $name
+     * @return array
+     */
+    public function metadata($name)
+    {
+        // MetaDataManager already caches this information.
+        return $this->mdm->metadata($name);
+    }
+
+    /**
+     * Prepares an Entity for use in the application.
+     * 
+     * @param \Rawebone\Ormish\Entity $entity
+     * @param \Rawebone\Ormish\GatewayInterface $gateway
+     * @param \Rawebone\Ormish\Database $db
+     * @param boolean $readOnly
+     */
+    public function prepare(Entity $entity, GatewayInterface $gateway, Database $db, $readOnly)
+    {
+        $shadow = (boolean)$readOnly ? new NullShadow() : new Shadow();
+        $shadow->update($entity->all());
+        
+        $entity->letDatabase($db);
+        $entity->letShadow($shadow);
+        $entity->letGateway($gateway);
+    }
+
+    public function create($name, $idField, array $initial)
+    {
+        $data = array_merge($this->defaults($name), $initial);
+
+        if (isset($data[$idField])) { // ID's should always be null on new instances
+            $data[$idField] = null;
+        }
+        
+        return $this->objects->create($name, $data);
+    }
+}
---
+++ spec/Rawebone/Ormish/EntityManagerSpec.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace spec\Rawebone\Ormish;
+
+use Rawebone\Ormish\Entity;
+use Rawebone\Ormish\Database;
+use Rawebone\Ormish\GatewayInterface;
+use Rawebone\Ormish\DefaultsCreator;
+use Rawebone\Ormish\MetaDataManager;
+use Rawebone\Ormish\ObjectCreator;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class EntityManagerSpec extends ObjectBehavior
+{
+    function let(DefaultsCreator $defaults, MetaDataManager $mdm, ObjectCreator $objects)
+    {
+        $this->beConstructedWith($defaults, $mdm, $objects);
+    }
+    
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Rawebone\Ormish\EntityManager');
+    }
+    
+    function it_should_return_metadata($mdm)
+    {
+        $mdm->metadata("name")->willReturn(1);
+        $this->metadata("name")->shouldReturn(1);
+    }
+    
+    function it_should_return_defaults($defaults, $mdm)
+    {
+        $mdm->metadata("name")->willReturn(array());
+        $defaults->make(array())->willReturn(1);
+        
+        $this->defaults("name")->shouldReturn(1);
+    }
+    
+    function it_should_prepare_an_entity_with_a_null_shadow(Entity $ent, 
+        GatewayInterface $gate, Database $db)
+    {
+        $readOnly = true;
+        
+        $ent->all()->willReturn(array())->shouldBeCalled();
+        $ent->letDatabase($db)->shouldBeCalled();
+        $ent->letGateway($gate)->shouldBeCalled();
+        $ent->letShadow(Argument::type('Rawebone\Ormish\NullShadow'))->shouldBeCalled();
+        
+        $this->prepare($ent, $gate, $db, $readOnly);
+    }
+    
+    function it_should_prepare_an_entity_with_a_real_shadow(Entity $ent, 
+        GatewayInterface $gate, Database $db)
+    {
+        $readOnly = false;
+        
+        $ent->all()->willReturn(array())->shouldBeCalled();
+        $ent->letDatabase($db)->shouldBeCalled();
+        $ent->letGateway($gate)->shouldBeCalled();
+        $ent->letShadow(Argument::type('Rawebone\Ormish\Shadow'))->shouldBeCalled();
+        
+        $this->prepare($ent, $gate, $db, $readOnly);
+    }
+    
+    function it_should_create_an_entity($defaults, $mdm, $objects)
+    {
+        $name = "test";
+        $idField = "id";
+        $values  = array("a" => "b");
+        
+        $mdm->metadata($name)->willReturn(array());
+        $defaults->make(array())->willReturn(array(
+            "id" => 0,
+            "a" => "",
+            "c" => ""
+        ));
+        
+        $objects->create($name, array("id" => null, "a" => "b", "c" => ""))->willReturn(true);
+        $this->create($name, $idField, $values)->shouldReturn(true);
+    }
+}
