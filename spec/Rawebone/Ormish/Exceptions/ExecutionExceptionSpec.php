b5fde2161ed89bddd562d4c0e88ad3bbd433a719
Nick Rawe <rawebone@gmail.com>
2014-03-10 22:40:35 +0000

    Fixed issue with failing example.

--- spec/Rawebone/Ormish/Exceptions/ExecutionExceptionSpec.php
+++ spec/Rawebone/Ormish/Exceptions/ExecutionExceptionSpec.php
@@ -7,11 +7,6 @@ use Prophecy\Argument;
 
 class ExecutionExceptionSpec extends ObjectBehavior
 {
-    function it_is_initializable()
-    {
-        $this->shouldHaveType('Rawebone\Ormish\Exceptions\ExecutionException');
-    }
-
     function it_should_encapsulate_error_state()
     {
         $code   = "0";
