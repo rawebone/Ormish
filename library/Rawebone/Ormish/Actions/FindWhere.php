676d91e3ef9bd0fb7cf6f4025a4cc18ae5103f39
Nick Rawe <nickrawe2@gmail.com>
2014-03-11 17:30:53 +0000

    Added the FindWhere Action

---
+++ library/Rawebone/Ormish/Actions/FindWhere.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Rawebone\Ormish\Actions;
+
+use Rawebone\Ormish\Exceptions\ExecutionException;
+
+class FindWhere extends AbstractAction
+{
+    public function run($condition)
+    {
+        $params = func_get_args();
+        array_shift($params); // Remove $condition from the parameters list
+
+        $query = $this->generator->findWhere($this->table->table(), $condition);
+
+        try {
+            $stmt = $this->executor->query($query, $params);
+        } catch (ExecutionException $ex) { // Already logged
+            return array();
+        }
+
+        $entities = $this->populator->populate($stmt, $this->table->model());
+        foreach ($entities as $entity) {
+            $this->entityManager->prepare(
+                $entity,
+                $this->gateway,
+                $this->database,
+                $this->table->readOnly()
+            );
+        }
+
+        return $entities;
+    }
+}
