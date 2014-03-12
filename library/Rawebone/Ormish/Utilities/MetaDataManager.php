b0c4363177b92fefef70ada2b55b6bb06802e2d7
Nick Rawe <rawebone@gmail.com>
2014-03-12 21:24:40 +0000

    Tables from Annotations, part 1
    Updated the MetaDataManager firstly to remove the dependency on phpDocumenter- this has been replaced with hand-written and more focused parsers.
    
    Secondly, this now returns an array with the parsed table annotations (if present).

--- library/Rawebone/Ormish/Utilities/MetaDataManager.php
+++ library/Rawebone/Ormish/Utilities/MetaDataManager.php
@@ -2,50 +2,129 @@
 
 namespace Rawebone\Ormish\Utilities;
 
-use phpDocumentor\Reflection\DocBlock;
-
+/**
+ * Returns MetaData about classes use in the library.
+ *
+ * We could fairly easily use one of the libraries designed
+ * to parse for annotations - however given the relative
+ * simplicity of what we want to achieve the overhead
+ * and complexity caused is unnecessary. As such handwritten,
+ * specific parsers are the way forward here.
+ */
 class MetaDataManager
 {
     protected $docBlockCache = array();
     protected $propertyCache = array();
-    protected $cache = array();
-    
+    protected $tableCache    = array();
+
     public function properties($class)
     {
         if (!isset($this->propertyCache[$class])) {
-            $this->cacheProperties($class);
+            $comment = $this->getDocBlock($class);
+            $properties = $this->parseDocBlockProperties($comment);
+
+            $this->propertyCache[$class] = $properties;
         }
         
         return $this->propertyCache[$class];
     }
-    
-    protected function cacheProperties($class)
+
+    public function table($class)
     {
-        $parser = $this->getDocBlock($class);
-        
+        if (!isset($this->tableCache[$class])) {
+            $comment = $this->getDocBlock($class);
+            $table   = $this->parseDocBlockForTable($comment);
+
+            $this->tableCache[$class] = $table;
+        }
+
+        return $this->tableCache[$class];
+    }
+
+    /**
+     * Returns the `property` tags from a DocBlock in the format
+     * <type, name>,
+     *
+     * @param $comment
+     * @return array
+     */
+    protected function parseDocBlockProperties($comment)
+    {
+        static $regex = "#@property (?<type>[a-zA-Z0-9\\\]+) \\$(?<name>[_a-zA-Z0-9]+)#";
+
+        if (empty($comment)
+            || ($count = preg_match_all($regex, $comment, $matches)) === 0) {
+            return array();
+        }
+
         $map = array();
-        foreach ($parser->getTagsByName("property") as $tag) {
-            $type = $tag->getType();
+        for ($i = 0; $i < $count; $i++) {
+            $type = $matches["type"][$i];
+            $name = $matches["name"][$i];
+
             while ($type[0] === '\\') {
                 $type = substr($type, 1);
             }
 
-            $map[str_replace("$", "", $tag->getVariableName())] = $type;
+            $map[$name] = $type;
         }
-        
-        $this->propertyCache[$class] = $map;
+
+        return $map;
     }
 
     /**
+     * Returns the details of the table annotations.
+     *
+     * @param string $comment
+     * @return array
+     */
+    protected function parseDocBlockForTable($comment)
+    {
+        if (empty($comment)) {
+            return array();
+        }
+
+        $table = array(
+            "table" => $this->getValueForTag("table", $comment),
+            "primaryKey" => $this->getValueForTag("primaryKey", $comment),
+            "softDelete" => (preg_match("#@softDelete#", $comment) === 1),
+            "readOnly" => (preg_match("#@softDelete#", $comment) === 1)
+        );
+
+        return $table;
+    }
+
+    /**
+     * Returns the string from the DocComment of the class.
+     *
      * @param string $class The class name to get the parser for.
-     * @return \phpDocumentor\Reflection\DocBlock
+     * @return string
      */
     protected function getDocBlock($class)
     {
         if (!isset($this->docBlockCache[$class])) {
-            $this->docBlockCache[$class] = new DocBlock(new \ReflectionClass($class));
+            $reflection = new \ReflectionClass($class);
+            $this->docBlockCache[$class] = $reflection->getDocComment();
         }
 
         return $this->docBlockCache[$class];
+    }
+
+    /**
+     * Where we expect to have a tag followed by a string and a new line,
+     * return that string as the value for the tag.
+     *
+     * @param string $name
+     * @param string $comment
+     */
+    protected function getValueForTag($name, $comment)
+    {
+        $regex = "#@{$name} (?<value>[_a-zA-Z0-9]+)#";
+
+        if (preg_match($regex, $comment, $match) == 0) {
+            return null;
+        }
+
+        return $match["value"];
     }
 }
--- spec/Rawebone/Ormish/Utilities/MetaDataManagerSpec.php
+++ spec/Rawebone/Ormish/Utilities/MetaDataManagerSpec.php
@@ -8,6 +8,11 @@ use Prophecy\Argument;
 /**
  * @property int $test This is a test var to make sure the manager behaves correctly
  * @property \DateTime $testDate This is a test var to make sure the manager behaves correctly
+ *
+ * @table my_table
+ * @primaryKey id
+ * @softDelete
+ * @readOnly
  */
 class MetaDataManagerSpec extends ObjectBehavior
 {
@@ -20,5 +25,16 @@ class MetaDataManagerSpec extends ObjectBehavior
     {
         $this->properties(__CLASS__)
              ->shouldReturn(array("test" => "int", "testDate" => "DateTime"));
+    }
+
+    function it_should_return_table_information()
+    {
+        $this->table(__CLASS__)
+             ->shouldReturn(array(
+                "table" => "my_table",
+                "primaryKey" => "id",
+                "softDelete" => true,
+                "readOnly" => true
+            ));
     }
 }
