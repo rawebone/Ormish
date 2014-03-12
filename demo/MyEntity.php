247967af2e30f604ca06b708b7d6e4613c5d64e9
Nick Rawe <rawebone@gmail.com>
2014-03-12 22:46:17 +0000

    Added the Demo
    This will allow for users to easily see how the functionality works and also to provide an
    informal functional test suite so that performance and flow can be monitored.

---
+++ demo/MyEntity.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This is an Entity - an object which represents your data in the database,
+ * it's relationship to other Entities and any business rules required at
+ * the base data layer.
+ *
+ * The library takes care of handling access to data, leaving you to focus
+ * on any relationships and business rules: You only have to provide it with
+ * information so that it can work for you. That information is the parameters
+ * you expect on the model (both for IDE hinting and use in creating defaults
+ * and casting values) and information about the table the entity links to.
+ *
+ * Properties:
+ *
+ * @property int $id
+ * @property bool $deleted
+ * @property float $my_float
+ * @property string $aString
+ * @property \DateTime $created Ormish automatically converts data in the converted field to a DateTime and back
+ *
+ * Table:
+ *
+ * @primaryKey id
+ * @table my_entities
+ * @softDelete
+ * @readOnly
+ *
+ * "softDelete" means that 'deleted' will be set to 1 instead of a deletion being issued.
+ * "readOnly" prevents deletions, inserts or updates being made on the table.
+ */
+class MyEntity extends \Rawebone\Ormish\Entity
+{
+}
---
+++ demo/demo.php
@@ -0,0 +1,88 @@
+<?php
+
+/////////////////////////////////////////
+///
+/// Ormish Demo
+///
+/////////////////////////////////////////
+
+require_once __DIR__ . "/../vendor/autoload.php";
+include_once __DIR__ . "/MyEntity.php";
+
+build_demo_database(($db = __DIR__ . "/demo.db"));
+
+
+/// Initialisation
+/// ==============
+///
+/// To make it easy to get up and off the ground with the library
+/// a factory is included. It has more options than covered here
+/// for allowing you to specify your own SQL Generators (for example)
+/// but the long and short is that you pass through you your server
+/// details and it gives you a configured Ormish database connection.
+
+$factory  = new \Rawebone\Ormish\Factory("sqlite:$db", "", "");
+$database = $factory->build();
+
+
+/// General Usage
+/// =============
+///
+/// The basic pattern of usage is via an OOP API.
+
+/// Firstly, we have to expose our Entities to the Database
+$database->attach('MyEntity');
+
+/// Now, we can create a new Entity through the API
+$created = $database->get('MyEntity')->create(array("aString" => "blah"));
+$created->my_float = 1.0; // Amend the values
+$created->save();
+
+/// Now we can use the finding methods built into the gateway
+$found = $database->get('MyEntity')->find(1);
+$found->delete();
+
+/// We can also carry out work in a transaction so that if
+/// one operation fails the rest of the actions rollback
+(new \Rawebone\Ormish\Transaction($database->getExecutor(), function () use ($database) {
+
+    $found = $database->get('MyEntity')->findWhere("aString = ?", "bingo");
+    foreach ($found as $entity) {
+        $entity->delete();
+    }
+
+}))->run();
+
+
+/// Globals mode
+/// ============
+///
+/// This pattern of usage is not recommend but supported
+/// as it is sometimes more convenient and easier to
+/// rationalise usage of the Entities.
+
+/// We create the database in the same way, and pass this
+/// through to the Entity
+\Rawebone\Ormish\Entity::globalDatabase($database);
+
+/// We can then access the gateway actions via static calls
+$entity = MyEntity::find(1);
+
+
+function build_demo_database($file)
+{
+    touch($file);
+    $pdo = new \PDO("sqlite:$file", "", "");
+
+    // Build the table
+    $sql = "CREATE DATABASE my_entities (" .
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, " .
+        "deleted INTEGER, " .
+        "my_float REAL, " .
+        "aString TEXT, " .
+        "created TEXT)";
+
+    $pdo->exec($sql);
+}
+
+unlink($db);
